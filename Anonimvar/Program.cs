d59c8686a4f12dac9d2523d31cd99096e6feb449
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-19 18:42:41 +0300

    Добавьте файлы проекта.

---
+++ Anonimvar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+namespace Запити
+{
+    class Програма
+    {
+        static void Main(string[] args)
+        {
+            int[] масив = { 15, 24, 27, 12, 44, 12, 21, 108, 25, 33, 41 };
+            var запит = from i in масив
+                                     select i;
+            WriteLine("Усі елементи джерела даних: ");
+            foreach (var елемент in запит)
+            {
+                Write($"{елемент}\t");
+            }
+            WriteLine();
+            масив[0] = 5;
+            foreach (var елемент in запит)
+            {
+                Write($"{елемент}\t");
+            }
+        }
+    }
+}
